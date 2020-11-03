1076cc254c4ffa754de3fa8ac9b2b3134932da5f
megeep <64414394+megeep@users.noreply.github.com>
2020-11-03 09:09:55 -0700

    Add files via upload

--- scratch.py
+++ scratch.py
@@ -119,15 +119,18 @@ while True:
         # elkBois = thirstyBois + ['rileygelatt@gmail.com']
         # deerBois = ['megeep@gmail.com', 'shco6527@gmail.com', 'a.lodolce@gmail.com', 'colinstewrat@gmail.com',
         #                     '7209365042@vtext.com', '3034374578@messaging.sprintpcs.com']
-        testBois = ['megeep@gmail.com', 'colinstewrat@gmail.com',
-                    '7209365042@vtext.com', '3034374578@messaging.sprintpcs.com']
+        testBois = ['megeep@gmail.com', 'colinstewrat@gmail.com', 'rileygelatt@gmail.com',
+                    'a.lodolce@gmail.com', '7209365042@vtext.com', '3034374578@messaging.sprintpcs.com',
+                    '9707788390@messaging.sprintpcs.com']
         # Makes a dictionary object. The tag code is the key, while the email list is the value. Use the key to retrieve the value
         tagDict = {"DF012O3R", "DF015O3R", "DF025O3R", "DF035O3R", "DF044O3R", "DF049O3R", "DF444O3R", "DF501O3R",
                    "DF511O3R", "DM006O3R", "DM012O3R", "DM015O3R", "DM025O3R", "DM031O3R", "DM035O3R", "DM043O3R",
                    "DM044O3R", "DM049O3R", "DM050O3R", "DM051O3R", "DM055O3R", "DM059O3R", "DM068O3R", "DM069O3R",
                    "DM082O3R", "DM085O3R", "DM140O3R", "DM161O3R", "DM171O3R", "DM444O3R", "DM551O3R", "DE131P3R",
                    "DM131O3R", "EF012O3R", "EF014O3R", "EF016O3R", "EF032O3R", "EF042O3R", "EF161O3R", "EF231O3R",
-                   "EF471O3R", "EF500O3R"}
+                   "EF471O3R", "EF500O3R","EF007O3R", "EF020O3R", "EF048O3R", "EF049O3R", "EF049S3R", "EF055O3R",
+                   "EF056O3R", "EF551O3R", "EM020O3R", "EM039O3R", "EM048O3R", "EM049O3R", "EM050O3R", "EM056O3R",
+                   "EM057O3R", "EM069O3R", "EM500O3R", "EM501O3R"}
 
         #TestTag , "EF131P3R"
         # EF131P3R = {"List": "TEST TAG", "Drawn At": "Leftover", "%Draw": "", "Success": "", "3 yr Avg%": "", "OTC ELK": "Y",
@@ -303,7 +306,79 @@ while True:
                     "OTC ELK": "N", "%Public": "0.73", "ANY BULL": "Y", "Driving (hr)": "1.3", "#Tags": "83",
                     "Public Per Tag": "", "Leftover Day": "0", "3 year leftover day avg": "0", "Valid Units": "",
                     "Sq Mi Public": "", "Bull to Cow": "36", }
-
+        EF007O3R = {"List": "A", "Drawn At": "0", "%Draw": "0.05", "Success": "0.28", "3 yr Avg%": "0.3",
+                    "OTC ELK": "N", "%Public": "0.87", "ANY BULL": "N", "Driving (hr)": "2.5", "#Tags": "150",
+                    "Public Per Tag": "", "Leftover Day": "0", "3 year leftover day avg": "0", "Valid Units": "",
+                    "Sq Mi Public": "", "Bull to Cow": "24", }
+        EF020O3R = {"List": "A", "Drawn At": "0", "%Draw": "0.33", "Success": "0.4", "3 yr Avg%": "0.24",
+                    "OTC ELK": "N", "%Public": "0.48", "ANY BULL": "Y", "Driving (hr)": "1", "#Tags": "20",
+                    "Public Per Tag": "", "Leftover Day": "0", "3 year leftover day avg": "0", "Valid Units": "",
+                    "Sq Mi Public": "", "Bull to Cow": "47", }
+        EF048O3R = {"List": "A", "Drawn At": "0", "%Draw": "0.16", "Success": "0.65", "3 yr Avg%": "0.46",
+                    "OTC ELK": "N", "%Public": "0.89", "ANY BULL": "Y", "Driving (hr)": "2", "#Tags": "17",
+                    "Public Per Tag": "", "Leftover Day": "0", "3 year leftover day avg": "0", "Valid Units": "",
+                    "Sq Mi Public": "", "Bull to Cow": "26", }
+        EF049O3R = {"List": "A", "Drawn At": "1", "%Draw": "0.28", "Success": "0.33", "3 yr Avg%": "0.27",
+                    "OTC ELK": "N", "%Public": "0.73", "ANY BULL": "Y", "Driving (hr)": "1.9", "#Tags": "110",
+                    "Public Per Tag": "", "Leftover Day": "0", "3 year leftover day avg": "1", "Valid Units": "",
+                    "Sq Mi Public": "", "Bull to Cow": "31", }
+        EF049S3R = {"List": "A", "Drawn At": "Choice 2", "%Draw": "0.12", "Success": "0.38", "3 yr Avg%": "0.38",
+                    "OTC ELK": "N", "%Public": "0.73", "ANY BULL": "Y", "Driving (hr)": "1.9", "#Tags": "50",
+                    "Public Per Tag": "", "Leftover Day": "1", "3 year leftover day avg": "0.333333333333333",
+                    "Valid Units": "", "Sq Mi Public": "", "Bull to Cow": "31", }
+        EF055O3R = {"List": "A", "Drawn At": "1", "%Draw": "0.83", "Success": "0.08", "3 yr Avg%": "0.26",
+                    "OTC ELK": "Y", "%Public": "0.89", "ANY BULL": "N", "Driving (hr)": "3", "#Tags": "100",
+                    "Public Per Tag": "", "Leftover Day": "4", "3 year leftover day avg": "3.66666666666667",
+                    "Valid Units": "", "Sq Mi Public": "", "Bull to Cow": "24", }
+        EF056O3R = {"List": "A", "Drawn At": "Choice 4", "%Draw": "0.17", "Success": "0.11", "3 yr Avg%": "0.26",
+                    "OTC ELK": "N", "%Public": "0.68", "ANY BULL": "Y", "Driving (hr)": "2.5", "#Tags": "55",
+                    "Public Per Tag": "", "Leftover Day": "0", "3 year leftover day avg": "0", "Valid Units": "",
+                    "Sq Mi Public": "", "Bull to Cow": "26", }
+        EF551O3R = {"List": "A", "Drawn At": "1", "%Draw": "0.94", "Success": "0.6", "3 yr Avg%": "0.41",
+                    "OTC ELK": "Y", "%Public": "0.86", "ANY BULL": "N", "Driving (hr)": "3.2", "#Tags": "70",
+                    "Public Per Tag": "", "Leftover Day": "0", "3 year leftover day avg": "2.33333333333333",
+                    "Valid Units": "", "Sq Mi Public": "", "Bull to Cow": "24", }
+        EM020O3R = {"List": "A", "Drawn At": "4", "%Draw": "1", "Success": "0.44", "3 yr Avg%": "0.55", "OTC ELK": "N",
+                    "%Public": "0.48", "ANY BULL": "Y", "Driving (hr)": "1", "#Tags": "25", "Public Per Tag": "23.136",
+                    "Leftover Day": "0", "3 year leftover day avg": "0", "Valid Units": "20", "Sq Mi Public": "578.4",
+                    "Bull to Cow": "47", }
+        EM039O3R = {"List": "A", "Drawn At": "Choice 3", "%Draw": "0.03", "Success": "0.28", "3 yr Avg%": "0.26",
+                    "OTC ELK": "N", "%Public": "0.65", "ANY BULL": "Y", "Driving (hr)": "0.9", "#Tags": "50",
+                    "Public Per Tag": "4.797", "Leftover Day": "3", "3 year leftover day avg": "1.33333333333333",
+                    "Valid Units": "39", "Sq Mi Public": "239.85", "Bull to Cow": "33", }
+        EM048O3R = {"List": "A", "Drawn At": "1", "%Draw": "0.84", "Success": "0.3", "3 yr Avg%": "0.29",
+                    "OTC ELK": "N", "%Public": "0.89", "ANY BULL": "Y", "Driving (hr)": "2", "#Tags": "16",
+                    "Public Per Tag": "16.6875", "Leftover Day": "0", "3 year leftover day avg": "0",
+                    "Valid Units": "48", "Sq Mi Public": "267", "Bull to Cow": "26", }
+        EM049O3R = {"List": "A", "Drawn At": "4", "%Draw": "0.41", "Success": "0.3", "3 yr Avg%": "0.36",
+                    "OTC ELK": "N", "%Public": "0.73", "ANY BULL": "Y", "Driving (hr)": "1.9", "#Tags": "34",
+                    "Public Per Tag": "11.5511764705882", "Leftover Day": "0", "3 year leftover day avg": "0",
+                    "Valid Units": "49", "Sq Mi Public": "392.74", "Bull to Cow": "31", }
+        EM050O3R = {"List": "A", "Drawn At": "1", "%Draw": "0.89", "Success": "0.34", "3 yr Avg%": "0.28",
+                    "OTC ELK": "N", "%Public": "0.49", "ANY BULL": "Y", "Driving (hr)": "1.7", "#Tags": "64",
+                    "Public Per Tag": "3.828125", "Leftover Day": "0", "3 year leftover day avg": "0",
+                    "Valid Units": "50", "Sq Mi Public": "245", "Bull to Cow": "36", }
+        EM056O3R = {"List": "A", "Drawn At": "0", "%Draw": "0.62", "Success": "0.13", "3 yr Avg%": "0.24",
+                    "OTC ELK": "N", "%Public": "0.68", "ANY BULL": "Y", "Driving (hr)": "2.5", "#Tags": "45",
+                    "Public Per Tag": "3.64177777777778", "Leftover Day": "1",
+                    "3 year leftover day avg": "0.333333333333333", "Valid Units": "56", "Sq Mi Public": "163.88",
+                    "Bull to Cow": "26", }
+        EM057O3R = {"List": "A", "Drawn At": "2", "%Draw": "0.04", "Success": "0.34", "3 yr Avg%": "0.25",
+                    "OTC ELK": "N", "%Public": "0.74", "ANY BULL": "Y", "Driving (hr)": "2.2", "#Tags": "80",
+                    "Public Per Tag": "8.50975", "Leftover Day": "0", "3 year leftover day avg": "0.666666666666667",
+                    "Valid Units": "58,57", "Sq Mi Public": "680.78", "Bull to Cow": "31", }
+        EM069O3R = {"List": "A", "Drawn At": "3", "%Draw": "0.56", "Success": "0.57", "3 yr Avg%": "0.48",
+                    "OTC ELK": "N", "%Public": "0.35", "ANY BULL": "Y", "Driving (hr)": "2.3", "#Tags": "40",
+                    "Public Per Tag": "14.955", "Leftover Day": "0", "3 year leftover day avg": "0",
+                    "Valid Units": "84,69", "Sq Mi Public": "598.2", "Bull to Cow": "39", }
+        EM500O3R = {"List": "A", "Drawn At": "0", "%Draw": "0.21", "Success": "0.3", "3 yr Avg%": "0.31",
+                    "OTC ELK": "N", "%Public": "0.73", "ANY BULL": "Y", "Driving (hr)": "1.3", "#Tags": "9",
+                    "Public Per Tag": "13.14", "Leftover Day": "0", "3 year leftover day avg": "0",
+                    "Valid Units": "500", "Sq Mi Public": "118.26", "Bull to Cow": "36", }
+        EM501O3R = {"List": "A", "Drawn At": "2", "%Draw": "0.76", "Success": "0.47", "3 yr Avg%": "0.4",
+                    "OTC ELK": "N", "%Public": "0.9", "ANY BULL": "Y", "Driving (hr)": "1.5", "#Tags": "25",
+                    "Public Per Tag": "18.036", "Leftover Day": "0", "3 year leftover day avg": "0",
+                    "Valid Units": "501", "Sq Mi Public": "450.9", "Bull to Cow": "36", }
 
         # For each page extract text and do the search for tag codes
         for i in range(0, NumPages):
